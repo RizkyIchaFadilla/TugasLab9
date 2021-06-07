6e5053768e750b48419bef7842264a008c3576f2
RizkyIchaFadilla <79885060+RizkyIchaFadilla@users.noreply.github.com>
2021-06-07 11:04:13 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,68 @@
+//Soal nomer 1 
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        new A();
+        new B();
+    }
+
+    static class A {
+        int i = 7;
+
+        public A() {
+            setI(20);
+            System.out.println("i from A is " + i);
+        }
+
+        public void setI(int i) {
+            this.i = 2 * i;
+        }
+    }
+
+    static class B extends A {
+        public B() {
+            System.out.println("i from B is " + i);
+        }
+
+        public void setI(int i) {
+            this.i = 3 * i;
+        }
+
+    }
+}
+
+//Soal nomer 2
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        new Person().printPerson();
+        new Student().printPerson();
+
+    }
+    static class Student extends Person {
+        @Override
+        public String getInfo() {
+            return "Student";
+        }
+    }
+    static class Person {
+        public String getInfo() {
+            return "Person";
+        }
+        public void printPerson() {
+            System.out.println(getInfo());
+        }
+    }
+}
+
+//Soal nomer 3
+
 package com.company;
 
 public class Main {
