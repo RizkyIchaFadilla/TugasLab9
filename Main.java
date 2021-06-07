b9d1f6c9cf33484f206cd0ddd4c56d0f34a70d36
RizkyIchaFadilla <79885060+RizkyIchaFadilla@users.noreply.github.com>
2021-06-07 10:54:27 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        new Person().printPerson();
+        new Student().printPerson();
+    }
+}
+
+class Student extends Person {
+    private String getInfo() {
+        return "Student";
+    }
+}
+
+class Person {
+    private String getInfo() {
+        return "Person";
+    }
+
+    public void printPerson() {
+        System.out.println(getInfo());
+    }
+}
